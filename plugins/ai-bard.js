41dc1dee67aa0a0847120c837915e165d6ceb80f
E D E R <139829285+WOTCHITO@users.noreply.github.com>
2024-08-05 18:15:17 -0500

    Update ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -1,26 +1,22 @@
 import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
-
-if (!text) return conn.reply(m.chat, `🎌 *Ingresé una petición*\n\nEjemplo, !bard Conoces CuriosityBot-MD?`, m, fake, )
-
+if (!text) return conn.reply(m.chat, `🍟 *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces a Ai-Yaemori?`, m, rcanal)
 try {
-
+await m.react('🕒')
 conn.sendPresenceUpdate('composing', m.chat)
 var apii = await fetch(`https://aemt.me/bard?text=${text}`)
 var res = await apii.json()
-await m.reply(res.result)
-
+await conn.reply(m.chat, res.result, m, rcanal)
+await m.react('✅️')
 } catch (error) {
+await m.react('✖️')
 console.error(error)
-return conn.reply(m.chat, `*🚩 Ocurrió un fallo*`, m, fake, )
-}
+return conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, rcanal)
+}}
 
-}
 handler.command = ['bard']
 handler.help = ['bard']
 handler.tags = ['ai']
-
 handler.premium = false
-
 export default handler
